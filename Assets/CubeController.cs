91754150e7951227f7f39ae6b8378de8a434c895
Masaki0509 <76939051+Masaki0509@users.noreply.github.com>
2021-02-23 14:48:27 +0900

    Initial commit

---
+++ Assets/CubeController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour
+{
+
+    // キューブの移動速度
+    private float speed = -12;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //課題提出用
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.tag == "CubeTag")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        if (other.collider.tag == "GroundTag")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
